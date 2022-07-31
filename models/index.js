e7fd52d5138654f84f9b879910fd44eb3a0904c0
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-31 09:53:40 -0700

    updated belongs to many, added model

--- models/index.js
+++ models/index.js
@@ -11,17 +11,21 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "product_id",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: "product_tag",
+  through: {
+    model: ProductTag,
+  },
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: "product_tag",
+  through: {
+    model: ProductTag,
+  },
 });
 
 module.exports = {
