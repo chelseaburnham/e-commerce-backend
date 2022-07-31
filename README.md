dfa753b4bddd9622cbc0ed6959b797694c7d90f7
Chelsea Burnham <chelseaburnham0@gmail.com>
2022-07-31 10:13:17 -0700

    udpatedc get and delete routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # e-commerce-backend
+
+Do I need Model: ProductTag in product-routes.js on lines 12 and 26?
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,53 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Product.findByPK(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that ID!" })
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+//update
+  
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+//update
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that ID!" })
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
